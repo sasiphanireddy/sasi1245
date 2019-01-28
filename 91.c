82032982613a7c4c294f54ec401b6523baec8722
sasiphanireddy <42197611+sasiphanireddy@users.noreply.github.com>
2019-01-28 21:47:29 +0530

    Create 91.c

---
+++ 91.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    float l,b,area;
+    printf("enter the length \n");
+    scanf("%f",&l);
+        printf("enter the breadth \n");
+    scanf("%f",&b);
+    area=l*b;
+    printf("%.5f area",area);
+    return 0;
+}
