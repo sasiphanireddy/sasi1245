607d493e602d02fa3c73a17e69aa8c1df4f7db6e
sasiphanireddy <42197611+sasiphanireddy@users.noreply.github.com>
2019-02-09 02:17:09 -0800

    Create 24.c

---
+++ 24.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,i;
+    printf("enter the num");
+    scanf("%d",&a);
+    printf("enter the num");
+    scanf("%d",&b);
+    for(i=a+1;i<=b;i++)
+    {
+        if(i%2!=0)
+        {
+            printf("%d",i);
+        }
+    }
+    getch();
+}
