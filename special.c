ca771c9cc1dbff5adfbe00958c02f241ff81adc6
sasiphanireddy <42197611+sasiphanireddy@users.noreply.github.com>
2019-02-10 16:43:31 +0530

    Create special.c

---
+++ special.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    char a[100];
+    int i,count=0;
+    printf("enter the scentence \n");
+    gets(a);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(a[i]=='!'||a[i]=='@'||a[i]=='#'||a[i]=='$'||a[i]=='%'||a[i]=='&'||a[i]=='*')
+            {
+                count++;
+            }
+    }
+    printf("%d",count);
+    getch();
+}
