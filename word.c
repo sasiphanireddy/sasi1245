a7c0c42f75273bf15516494c210cd1b2a26cc97a
sasiphanireddy <42197611+sasiphanireddy@users.noreply.github.com>
2019-02-10 16:25:22 +0530

    Create word.c

---
+++ word.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    char a[100];
+    int i,count=1;
+    printf("enter the scentence \n");
+    gets(a);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(a[i]==" ")
+        {
+            count++;
+        }
+        
+    }
+    printf("%d",count);
+    getch();
+}
