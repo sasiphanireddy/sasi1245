5cc07d3b9d9938a90748db6e111750918da101c2
sasiphanireddy <42197611+sasiphanireddy@users.noreply.github.com>
2019-02-10 16:28:27 +0530

    Create space.c

---
+++ space.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    char a[100];
+    int i,count=0;
+    printf("enter the scentence \n");
+    scanf("%s",&a);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(a[i]==" ")
+        {
+            count++;
+        }
+        
+    }
+    printf("%d",count);
+    getch();
+}
