a9d08567c187a1e6c176229fc7be2d7f864a282d
sasiphanireddy <42197611+sasiphanireddy@users.noreply.github.com>
2019-02-11 02:50:52 -0800

    Create 49.c

---
+++ 49.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a,b,sum;
+printf("enter the number \n");
+scanf("%d",&a);
+printf("enter the number \n");
+scanf("%d",&b);
+sum=a+b;
+
+printf(" %d \n",sum);
+getch();
+}
