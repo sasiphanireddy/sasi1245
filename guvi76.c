92f43205e65f7b649066fe6fba6a9d624d55e407
sasiphanireddy <42197611+sasiphanireddy@users.noreply.github.com>
2019-02-19 02:27:23 -0800

    Create guvi76.c

---
+++ guvi76.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i,des=0;
+scanf("%d",&n);
+for(i=2;i<=n/2;i++)
+{
+
+if(n%i==0)
+{
+
+des=1;
+break;
+}
+}
+if(des==0)
+{
+printf("no");
+}
+else
+{
+printf("yes");
+}
+getch();
+}
