67c85f2e8d36f6f229f6ebc7a95dfc5423bc5eee
sasiphanireddy <42197611+sasiphanireddy@users.noreply.github.com>
2019-02-19 02:18:17 -0800

    Create guvi72.c

---
+++ guvi72.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a[20];
+int i=0,cou=0,d=0;
+clrscr();
+gets(a);
+while(a[i]!='\0')
+{
+i++;
+cou++;
+}
+for(i=0;i<cou;i++)
+{
+if(a[i]>='a' && a[i]<='z')
+{
+d=0;
+}
+else
+{
+d=1;
+break;
+}
+}
+if(d==0)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+getch();
+}
