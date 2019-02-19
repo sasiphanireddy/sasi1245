5ce7edd47b8ed49abd5d6b4e713160f8c1db7a0e
sasiphanireddy <42197611+sasiphanireddy@users.noreply.github.com>
2019-02-19 02:04:36 -0800

    Update guvi66.c

--- guvi66.c
+++ guvi66.c
@@ -2,17 +2,17 @@
 #include<conio.h>
 void main()
 {
-int n,i,des=0;
+int n,i,d=0;
 scanf("%d",&n);
 for(i=2;i<=n/2;i++)
 {
 if(n%i==0)
 {
-des=1;
+d=1;
 break;
 }
 }
-if(des==0)
+if(d==0)
 {
 printf("yes");
 }
